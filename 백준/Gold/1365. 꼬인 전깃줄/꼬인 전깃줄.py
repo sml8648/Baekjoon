0b8e1a6dc345d71f9d603dca945cdc02af6357d7
sml8648 <sml8648@naver.com>
2022-08-06 08:07:17 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 304 ms, Memory: 41620 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+numbers = list(map(int, input().split()))
+answer = [0]
+
+count = 0
+for idx, number in enumerate(numbers):
+    if answer[-1] < number:
+        answer.append(number)
+
+    else:
+        left = 0
+        right = len(answer)
+
+        while left < right:
+            mid = (right+left)//2
+            if answer[mid] < number:
+                left = mid+1
+            else:
+                right = mid
+        answer[right] = number
+
+print(len(numbers) - (len(answer)-1))
