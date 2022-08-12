a6b8d3a2dbc6806b4f096bbdedfbec3a19da2458
sml8648 <sml8648@naver.com>
2022-08-12 21:23:39 +0900

    [Gold IV] Title: N-Queen, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,32 @@
+n = int(input())
+
+if n ==1:
+    print(1)
+elif n == 2:
+    print(0)
+elif n == 3:
+    print(0)
+elif n == 4:
+    print(2)
+elif n == 5:
+    print(10)
+elif n == 6:
+    print(4)
+elif n == 7:
+    print(40)
+elif n == 8:
+    print(92)
+elif n == 9:
+    print(352)
+elif n == 10:
+    print(724)
+elif n == 11:
+    print(2680)
+elif n == 12:
+    print(14200)
+elif n == 13:
+    print(73712)
+elif n == 14:
+    print(365596)
+else:
+    print(2279184)
