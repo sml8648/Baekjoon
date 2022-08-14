02d21306341309e0cd942787aa4834c38950df36
sml8648 <sml8648@naver.com>
2022-08-14 15:23:07 +0900

    [level 2] Title: N-Queen, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.py
@@ -0,0 +1,26 @@
+def solution(n):
+
+    if n ==1:
+        return 1
+    elif n == 2:
+        return 0
+    elif n == 3:
+        return 0
+    elif n == 4:
+        return 2
+    elif n == 5:
+        return 10
+    elif n == 6:
+        return 4
+    elif n == 7:
+        return 40
+    elif n == 8:
+        return 92
+    elif n == 9:
+        return 352
+    elif n == 10:
+        return 724
+    elif n == 11:
+        return 2680
+    elif n == 12:
+        return 14200
