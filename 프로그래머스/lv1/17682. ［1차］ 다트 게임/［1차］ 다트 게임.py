0481b92e67f1b396e67ce66c5b325a2fc211ba90
sml8648 <sml8648@naver.com>
2022-08-14 21:35:37 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,32 +1,26 @@
+a_list = "1T2D3D#"
 def solution(a_list):
     bonus = ['S','D','T']
     bonus_dict = {'S':1,'D':2,'T':3}
     option = ['*','#']
-    digit_list = ['0','1','2','3','4','5','6','7','8','9','A']
-    a_list = a_list.replace('10','A')
-    
-    result_list = []
-    for a, b in zip(a_list,a_list[1:]):
-        
-        if a in digit_list and b in bonus:
-            
-            if a == 'A':
-                a = 10
-                
+
+    result_list =[]
+    for a,b in zip(a_list, a_list[1:]):
+
+        if a.isdigit() and (b in bonus):
             tmp = int(a)**bonus_dict[b]
             result_list.append(tmp)
-        
+            print(result_list)
+        elif a.isdigit() and 
         elif b in option:
             if b == '*':
-                
-                if len(result_list) == 1:
-                    result_list[-1] *= 2
-                else:
+                try:
                     result_list[-1] *= 2
                     result_list[-2] *= 2
+                except:
+                    result_list[-1] *= 2
             else:
                 result_list[-1] = -result_list[-1]
-    
+
     print(result_list)
-    return sum(result_list)
-                    
+solution(a_list)
