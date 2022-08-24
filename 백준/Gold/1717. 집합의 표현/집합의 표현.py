b34b8d1c558edfcc5c6d213bc312381ffa81578c
sml8648 <sml8648@naver.com>
2022-08-24 22:48:26 +0900

    [Gold IV] Title: 집합의 표현, Time: 444 ms, Memory: 120444 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10000000)
+
+def find_parent(parent,x):
+
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int,input().split())
+parent = [0]*(v+1)
+
+for i in range(1, v+1):
+    parent[i] = i
+
+for i in range(e):
+    type,a, b = map(int,input().split())
+
+    if not type:
+        union_parent(parent,a,b)
+    else:
+
+        if find_parent(parent,a) == find_parent(parent,b):
+            print('YES')
+        else:
+            print('NO')
