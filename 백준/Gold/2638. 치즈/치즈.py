8164dc1a0395a24b6a3392b258c57d4581b3d3a3
sml8648 <sml8648@naver.com>
2022-12-09 21:22:44 +0900

    [Gold III] Title: 치즈, Time: 584 ms, Memory: 34160 KB -BaekjoonHub

---
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+n,m = map(int,input().split())
+a = [list(map(int,input().split())) for i in range(n)]
+c = 0
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+def bfs():
+    de = deque()
+    de.append([0,0])
+    ch=[[-1]*m for i in range(n)]
+    ch[0][0] = 0
+
+    while de:
+
+        x, y = de.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if ch[nx][ny] == -1:
+                    if a[nx][ny] >= 1:
+                        a[nx][ny] += 1
+                    else:
+                        ch[nx][ny] = 0
+                        de.append([nx,ny])
+
+while True:
+    bfs()
+    cht = 0
+    for i in range(n):
+        for j in range(m):
+            if a[i][j] >= 3:
+                a[i][j] = 0
+                cht += 1
+            elif a[i][j] == 2:
+                a[i][j] = 1
+
+    if cht >= 1:
+        c += 1
+    else:
+        break
+
+
+print(c)
