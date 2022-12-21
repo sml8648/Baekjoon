4224d9d9e0091933aa5b973056d6f91a72171d68
sml8648 <sml8648@naver.com>
2022-12-22 08:55:04 +0900

    [Silver II] Title: 상자넣기, Time: 224 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,10 @@
+n = input()
+arr = list(map(int,input().split()))
+D = [1] * len(arr)
+
+for i in range(1,len(arr)):
+    for j in range(0,i):
+        if arr[i] > arr[j]:
+            D[i] = max(D[j] + 1, D[i])
+
+print(max(D))
