8e4ce15e8551ce5060aa23f9bd101870b50b184c
sml8648 <sml8648@naver.com>
2023-01-14 19:33:14 +0900

    [Gold V] Title: 강의실 배정, Time: 476 ms, Memory: 134740 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -2,23 +2,23 @@ import sys
 input = sys.stdin.readline
 
 import heapq
+
 n = int(input())
-
-q = []
-
-for i in range(n):
-    start, end = map(int, input().split())
-    q.append([start, end])
-
-q.sort()
+tmp_list = []
+for _ in range(n):
+    a, b = map(int,input().split())
+    tmp_list.append((a,b))
+tmp_list.sort()
 
 room = []
-heapq.heappush(room, q[0][1])
+heapq.heappush(room, tmp_list[0][1])
+
 for i in range(1, n):
-    if q[i][0] < room[0]:
-        heapq.heappush(room, q[i][1])
+
+    if room[0] > tmp_list[i][0]:
+        heapq.heappush(room, tmp_list[i][1])
     else:
         heapq.heappop(room)
-        heapq.heappush(room, q[i][1])
+        heapq.heappush(room, tmp_list[i][1])
 
 print(len(room))
