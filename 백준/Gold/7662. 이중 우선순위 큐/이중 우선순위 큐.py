d2f51737c522bdad399b20b96e3677c8553a1ce3
sml8648 <sml8648@naver.com>
2023-02-02 23:45:46 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 3772 ms, Memory: 350112 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,44 +1,33 @@
-import heapq
 import sys
 input = sys.stdin.readline
+import heapq
 
-K = int(input())
+def pop(heap):
+    while len(heap) > 0:
+        data, id = heapq.heappop(heap)
+        if not deleted[id]:
+            deleted[id] = True
+            return data
+    return None
 
-for _ in range(K):
-
-    n = int(input())
-    q_1 = []
-    q_2 = []
-    visited = [False]*n
-    for i in range(n):
-
-        a,b = input().split()
-
-        if a == 'I':
-            heapq.heappush(q_1,(int(b),i))
-            heapq.heappush(q_2,(-int(b),i))
-            visited[i] = True
-
-        else:
-            if b == '-1':
-                while q_1 and (visited[q_1[0][1]] == False):
-                    heapq.heappop(q_1)
-                if q_1:
-                    visited[q_1[0][1]] = False
-                    heapq.heappop(q_1)
-            else:
-                while q_2 and (visited[q_2[0][1]] == False):
-                    heapq.heappop(q_2)
-                if q_2:
-                    visited[q_2[0][1]] = False
-                    heapq.heappop(q_2)
-
-    while q_1 and (visited[q_1[0][1]] == False):
-        heapq.heappop(q_1)
-    while q_2 and (visited[q_2[0][1]] == False):
-        heapq.heappop(q_2)
-
-    if not q_1 or not q_2:
-        print('EMPTY')
+for _ in range(int(input())):
+    k = int(input())
+    min_heap = []
+    max_heap = []
+    current = 0
+    deleted = [False] * (k + 1)
+    for i in range(k):
+        command = input().split()
+        operator, data = command[0], int(command[1])
+        if operator == 'D':
+            if data == -1: pop(min_heap)
+            elif data == 1: pop(max_heap)
+        elif operator == 'I':
+            heapq.heappush(min_heap, (data, current))
+            heapq.heappush(max_heap, (-data, current))
+            current += 1
+    max_value = pop(max_heap)
+    if max_value == None: print("EMPTY")
     else:
-        print(-q_2[0][0], q_1[0][0])
+        heapq.heappush(min_heap, (-max_value, current))
+        print(-max_value, pop(min_heap))
