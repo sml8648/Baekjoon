7cd3712839b5d73bac5571f5c01341027b6df3a2
sml8648 <sml8648@naver.com>
2023-02-05 10:37:20 +0900

    [Gold III] Title: 과제, Time: 44 ms, Memory: 33324 KB -BaekjoonHub

---
+++ 백준/Gold/13904. 과제/과제.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+import heapq
+n = int(input())
+
+homework_list = []
+for each in range(n):
+    a, b = map(int,input().split())
+
+    homework_list.append((a,b))
+
+homework_list.sort(key=lambda x:x[0])
+
+q = []
+today = 1
+total_score = 0
+for each in homework_list:
+
+    due, score = each
+
+    if len(q) < due:
+        heapq.heappush(q,(score, due))
+        total_score += score
+    else:
+
+        score2, due2 = heapq.heappop(q)
+
+        if score > score2:
+            heapq.heappush(q, (score, due))
+            total_score -= score2
+            total_score += score
+        else:
+            heapq.heappush(q, (score2, due2))
+
+print(total_score)
