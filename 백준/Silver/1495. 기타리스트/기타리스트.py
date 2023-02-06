7e196013893ca47379129cdd28d9413c87f3e358
sml8648 <sml8648@naver.com>
2023-02-06 10:56:52 +0900

    [Silver I] Title: 기타리스트, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.py
+++ 백준/Silver/1495. 기타리스트/기타리스트.py
@@ -1,22 +1,31 @@
+import sys
+input = sys.stdin.readline
 n, s, m = map(int,input().split())
-array = list(map(int,input().split()))
+volume = list(map(int,input().split()))
 
-dp = [[0] * (m + 1) for _ in range(n + 1)]
+dp = [[0]*(m+1) for _ in range(n+1)]
+
 dp[0][s] = 1
 
-for i in range(1, n + 1):
-    for j in range(m+1):
-        if dp[i-1][j] == 0:
-            continue
-        if j - array[i - 1] >= 0:
-            dp[i][j - array[i - 1]] = 1
-        if j + array[i - 1] <= m:
-            dp[i][j + array[i - 1]] = 1
+for idx, each in enumerate(volume):
 
-result = -1
-for i in range(m, -1, -1):
-    if dp[n][i] == 1:
-        result = i
-        break
+    for idx2, k in enumerate(dp[idx]):
 
-print(result)
+        if k:
+
+            upper = idx2 + each
+            lower = idx2 - each
+
+            if upper <= m:
+                dp[idx+1][upper] = 1
+
+            if lower >= 0:
+                dp[idx+1][lower] = 1
+
+max_value = -1
+for idx, each in enumerate(dp[n]):
+    if each:
+        if idx > max_value:
+            max_value = idx
+
+print(max_value)
