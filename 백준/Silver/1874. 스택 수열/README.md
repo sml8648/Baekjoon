4881ddfbd7fdac9717cecfcda26e61a4fffd6f18
sml8648 <sml8648@naver.com>
2023-02-21 13:41:14 +0900

    [Silver II] Title: 스택 수열, Time: 192 ms, Memory: 32840 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/README.md
+++ 백준/Silver/1874. 스택 수열/README.md
@@ -1,10 +1,10 @@
-# [Silver III] 스택 수열 - 1874 
+# [Silver II] 스택 수열 - 1874 
 
 [문제 링크](https://www.acmicpc.net/problem/1874) 
 
 ### 성능 요약
 
-메모리: 122704 KB, 시간: 240 ms
+메모리: 32840 KB, 시간: 192 ms
 
 ### 분류
 
--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -2,23 +2,29 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-stack = []
+
+count = 1
+s = []
+temp = True
 answer = []
-current = 1
-
 for _ in range(n):
-    x = int(input())
-    while len(stack) == 0 or stack[-1] < x:
-        stack.append(current)
-        current += 1
+
+    a = int(input())
+
+    while count <= a:
+        s.append(count)
         answer.append('+')
+        count += 1
 
-    if stack[-1] == x:
-        stack.pop()
+    if s[-1] == a:
+        s.pop()
         answer.append('-')
-    else:
-        answer = ['NO']
-        break
 
-for x in answer:
-    print(x)
+    else:
+        temp = False
+
+if temp == False:
+    print('NO')
+else:
+    for each in answer:
+        print(each)
