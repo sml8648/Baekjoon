ed064b7008b9cafbd67a8751500ed246145d6b49
sml8648 <sml8648@naver.com>
2023-04-10 22:38:32 +0900

    [level 3] Title: 블록 이동하기, Time: 63.70 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/60063. 블록 이동하기/블록 이동하기.py
@@ -0,0 +1,94 @@
+from collections import deque
+
+idx = [[0,1,0,1],[1,0,1,0],[-1,0,-1,0],[0,-1,0,-1]]
+idx_horiz = [[-1,1,0,0],[1,1,0,0],[0,0,1,-1],[0,0,-1,-1]]
+idx_verti = [[1,-1,0,0],[1,1,0,0],[0,0,-1,1],[0,0,-1,-1]]
+
+def rotate_check(board,
+                 tx1,ty1,tx2,ty2,
+                 n_tx1,n_ty1,n_tx2,n_ty2):
+
+    min_x = min(tx1,tx2,n_tx1, n_tx2)
+    min_y = min(ty1,ty2,n_ty1, n_ty2)
+
+    for i in range(min_x,min_x+2):
+        for j in range(min_y, min_y+2):
+            if board[i][j]:
+                return False
+    return True
+
+def solution(board):
+
+    x1, y1 = 0,0
+    x2, y2 = 0,1
+    flag = 'h'
+    length = len(board)
+
+    visited = set()
+    visited.add((x1,y1,x2,y2))
+
+    q = deque()
+    q.append((0, x1, y1, x2, y2, flag))
+
+    while q:
+        count, tx1, ty1, tx2, ty2, direct = q.popleft()
+
+        if (tx1 == length -1 and ty1 == length - 1) or (tx2 == length - 1 and ty2 == length - 1):
+            return count
+
+        for each in idx:
+            n_tx1 = tx1 + each[0]
+            n_ty1 = ty1 + each[1]
+            n_tx2 = tx2 + each[2]
+            n_ty2 = ty2 + each[3]
+            new_direct = direct
+
+            coordinate = sorted([(n_tx1, n_ty1), (n_tx2, n_ty2)], key=lambda x: (x[0], x[1]))
+            n_tx1, n_ty1, n_tx2, n_ty2 = *coordinate[0], *coordinate[1]
+
+            if 0 <= n_tx1 < length and 0 <= n_ty1 < length and 0 <= n_tx2 < length and 0 <= n_ty2 < length \
+                and not board[n_tx1][n_ty1] and not board[n_tx2][n_ty2]:
+
+                if (n_tx1, n_ty1, n_tx2, n_ty2) not in visited:
+                    q.append((count + 1, n_tx1, n_ty1, n_tx2, n_ty2, new_direct))
+                    visited.add((n_tx1, n_ty1, n_tx2, n_ty2))
+
+        if direct == 'h':
+
+            for each in idx_horiz:
+                n_tx1 = tx1 + each[0]
+                n_ty1 = ty1 + each[1]
+                n_tx2 = tx2 + each[2]
+                n_ty2 = ty2 + each[3]
+                new_direct = 'v'
+
+                coordinate = sorted([(n_tx1, n_ty1), (n_tx2, n_ty2)], key=lambda x: (x[0], x[1]))
+                n_tx1, n_ty1, n_tx2, n_ty2 = *coordinate[0], *coordinate[1]
+
+                if 0 <= n_tx1 < length and 0 <= n_ty1 < length and 0 <= n_tx2 < length and 0 <= n_ty2 < length \
+                        and not board[n_tx1][n_ty1] and not board[n_tx2][n_ty2] \
+                        and rotate_check(board, tx1,ty1,tx2,ty2,n_tx1,n_ty1,n_tx2,n_ty2):
+
+                    if (n_tx1, n_ty1, n_tx2, n_ty2) not in visited:
+                        q.append((count + 1, n_tx1, n_ty1, n_tx2, n_ty2, new_direct))
+                        visited.add((n_tx1, n_ty1, n_tx2, n_ty2))
+
+        else:
+
+            for each in idx_verti:
+                n_tx1 = tx1 + each[0]
+                n_ty1 = ty1 + each[1]
+                n_tx2 = tx2 + each[2]
+                n_ty2 = ty2 + each[3]
+                new_direct = 'h'
+
+                coordinate = sorted([(n_tx1, n_ty1), (n_tx2, n_ty2)], key=lambda x: (x[0], x[1]))
+                n_tx1, n_ty1, n_tx2, n_ty2 = *coordinate[0], *coordinate[1]
+
+                if 0 <= n_tx1 < length and 0 <= n_ty1 < length and 0 <= n_tx2 < length and 0 <= n_ty2 < length \
+                        and not board[n_tx1][n_ty1] and not board[n_tx2][n_ty2] \
+                        and rotate_check(board, tx1,ty1,tx2,ty2,n_tx1,n_ty1,n_tx2,n_ty2):
+
+                    if (n_tx1, n_ty1, n_tx2, n_ty2) not in visited:
+                        q.append((count + 1, n_tx1, n_ty1, n_tx2, n_ty2, new_direct))
+                        visited.add((n_tx1, n_ty1, n_tx2, n_ty2))
