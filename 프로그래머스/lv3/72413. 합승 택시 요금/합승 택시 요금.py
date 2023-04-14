6edf2d8eec5395e2df2bf685585c5fba3fc04d13
sml8648 <sml8648@naver.com>
2023-04-14 20:06:54 +0900

    [level 3] Title: 합승 택시 요금, Time: 29.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,20 +1,52 @@
-def solution(n, s, a, b, fares):
-    ans = 200000001
-    cost = [[20000001] * (n+1) for _ in range(n+1)]
-    def floyd_warshall():
-        for k in range(1, n+1):
-            for i in range(1, n+1):
-                for j in range(1, n+1):
-                    if i == j:
-                        cost[i][j] = 0
-                    else:
-                        cost[i][j] = min(cost[i][k] + cost[k][j], cost[i][j])
-        
-    for i, j, c in fares:
-        cost[i][j] = c
-        cost[j][i] = c
-    floyd_warshall()
-        
-    for i in range(1, n+1):
-        ans = min(cost[s][i] + cost[i][a] + cost[i][b], ans)
-    return ans
+import heapq
+
+def dijikstra(start,graph, distance):
+
+    q = []
+    heapq.heappush(q,(0,start))
+    distance[start] = 0
+
+    while q:
+        cost, now = heapq.heappop(q)
+
+        if distance[now] < cost:
+            continue
+
+        for each in graph[now]:
+            next = each[0]
+            new_cost = cost + each[1]
+            if new_cost <= distance[next]:
+                distance[next] = new_cost
+                heapq.heappush(q, (new_cost, next))
+
+def solution(n,s,a,b,fares):
+
+    graph = [[] for _ in range(n+1)]
+
+    for each in fares:
+        graph[each[0]].append((each[1], each[2]))
+        graph[each[1]].append((each[0], each[2]))
+
+    distance = [float('inf') for _ in range(n + 1)]
+
+    result = []
+    for i in range(n+1):
+        if not i:
+            distance = [float('inf') for _ in range(n + 1)]
+            dijikstra(s,graph, distance)
+            first = distance[a]
+            second = distance[b]
+            result.append(first+second)
+
+
+        else:
+            distance = [float('inf') for _ in range(n + 1)]
+            dijikstra(s,graph,distance)
+            mid = distance[i]
+            distance = [float('inf') for _ in range(n + 1)]
+            dijikstra(i,graph,distance)
+            first = distance[a]
+            second = distance[b]
+            result.append(mid+first+second)
+
+    return min(result)
