d7df6fe0055f80f5242ad049955783b0dddedc9a
sml8648 <sml8648@naver.com>
2023-04-17 09:52:03 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.cpp
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -1,16 +1,27 @@
 #include <string>
+#include <vector>
 #include <iostream>
-
 using namespace std;
 
 int solution(int num, int k) {
+    
     string num_str = to_string(num);
-    int len = num_str.length();
-
-    for (int i = 0; i < len; i++) {
-        if (num_str[i] - '0' == k) {
-            return i + 1;
-        }
+    string k_str = to_string(k);
+    
+    char test = '1';
+    
+    if (test == k_str){
+        cout << "ssdfk" << endl;
     }
-    return -1;
+    
+    
+    int answer = -1;
+//     for (int i=0;i < num_str.length();i++){
+        
+//         cout << typeid(num_str[i]).name() << endl;
+//         cout << typeid(k_str).name() << endl;
+//     }
+    
+    return answer;
+    
 }
