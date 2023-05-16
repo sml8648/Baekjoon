9b5e6d0d4cb839c47fd51150b796bc6a17308035
sml8648 <sml8648@naver.com>
2023-05-16 19:11:16 +0900

    [Gold III] Title: 개미굴, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.py
+++ 백준/Gold/14725. 개미굴/개미굴.py
@@ -5,7 +5,6 @@ input = sys.stdin.readline
 class Node(object):
     def __init__(self, key, data=None):
         self.key = key
-        self.data = data
         self.children = {}
 
 
@@ -23,23 +22,6 @@ class Trie:
             if not char in current.children:
                 current.children[char] = Node(char)
             current = current.children[char]
-
-        current.data = string
-        return True
-
-    def search(self, string):
-        current = self.head
-
-        for char in string:
-            if char in current.children:
-                current = current.children[char]
-            else:
-                return False
-
-        if current.data == string:
-            return current.data
-        else:
-            return False
 
 n = int(input())
 trie = Trie()
