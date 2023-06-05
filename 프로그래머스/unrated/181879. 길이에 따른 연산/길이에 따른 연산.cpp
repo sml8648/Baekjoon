0124567005d705558e775f2311271be5cdd6644f
sml8648 <sml8648@naver.com>
2023-06-05 14:12:28 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    
+    if(num_list.size() >= 11)
+    {
+        for(auto num: num_list)
+        {
+            answer += num;
+        }
+    }
+    else
+    {
+        answer = 1;
+        for(auto num:num_list)
+        {
+            answer *= num;
+        }
+    }
+    return answer;
+}
