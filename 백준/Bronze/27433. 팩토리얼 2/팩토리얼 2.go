02450cfdc6f410870cd51a0ded5133df604d9f00
sml8648 <sml8648@naver.com>
2024-03-06 22:11:52 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 4 ms, Memory: 860 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	return n * factorial(n-1)
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+
+	result := factorial(n)
+
+	fmt.Println(result)
+
+}
